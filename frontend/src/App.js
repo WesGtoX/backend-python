933b3c3dd874544b52400f19e3095f500763fedc
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-11-11 01:07:47 -0300

    Adding new layout login and register to authentication works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,6 @@
-import React, { useState } from 'react'
-import './App.css'
-import Login from './components/login'
+import React from "react"
+import "./styles/global"
+import Routes from "./routes"
 
-function App() {
-
-  const [token, setToken] = useState('')
-
-  const userLogin = (tok) => {
-    setToken(tok)
-  }
-
-  return (
-    <div className="App">
-      <Login userLogin={userLogin} />
-    </div>
-  )
-}
-
-export default App;
+const App = () => <Routes />
+export default App
