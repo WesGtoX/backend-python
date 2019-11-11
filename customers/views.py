7dd8058f7cabad775b70cfd340c252c615e03559
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2019-11-11 01:05:18 -0300

    Update auth and layout image

--- customers/views.py
+++ customers/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAdminUser
 from django.contrib.auth.models import User
 from .models import Customers
 from .serializers import UsersSerializer, CustomersSerializer
@@ -12,3 +14,5 @@ class UsersViewSet(viewsets.ModelViewSet):
 class CustomersViewSet(viewsets.ModelViewSet):
     queryset = Customers.objects.all()
     serializer_class = CustomersSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAdminUser]
